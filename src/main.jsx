7daad352027e638088aa40ef2d769f34b1d4f10f
JudoNomiSon <Rongriff.gt@gatech.edu>
2026-02-25 21:51:03 -0500

    Add src/main.jsx - React entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AgileLeapHomepage from './pages/AgileLeapHomepage';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AgileLeapHomepage />
+  </React.StrictMode>
+);
